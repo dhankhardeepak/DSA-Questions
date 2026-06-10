d0cb94664c80540ad2cc7b65e062ef421a3c4b07
Singh <ddhankhar18061995@gmail.com>
2026-06-10 13:08:17 +0530

    comitting Questions

---
+++ Array/Majority element/Answer.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 1;
+        int element = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            if(count == 0){
+                count = 1;
+                element = nums[i];
+            }
+            else if(element != nums[i]){
+                count--;
+            }
+            else{
+                count++;
+            }
+        }
+
+        return element;
+    }
+}
